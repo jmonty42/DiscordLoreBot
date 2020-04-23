810ad8efd10acb841b413adcdbcf27335be954e5
J.J. Montgomery <jmonty42dev@gmail.com>
2020-04-22 18:11:38 -0700

    MVP
    Everything should be working here now. Responds to the master with
    the top X suggestions.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import discord
+import re
 
 TOKEN_FILE_NAME = "token.txt"
 MASTER_FILE_NAME = "master.txt"
@@ -25,6 +26,31 @@ def main():
     client.run(token)
 
 
+async def fetch_suggestions(server, response_channel, max=10):
+    suggestion_channel = [channel for channel in server.text_channels if channel.name == GUILD_CHANNEL_NAME][0]
+    print("Found the channel named " + suggestion_channel.name)
+    messages = await suggestion_channel.history(limit=1000).flatten()
+    print("Got messages from that channel.")
+    suggestions = []
+    for message in messages:
+        count = 0
+        for reaction in message.reactions:
+            if str(reaction) != "\U0001F44E":
+                count += reaction.count
+        suggestions.append(Suggestion(count, message.content))
+    suggestions.sort(key=lambda suggestion: suggestion.votes, reverse=True)
+    response_string = ""
+    for rank in range(len(suggestions)):
+        response_string += str(rank+1) + ": " + suggestions[rank].name + ": (" + str(suggestions[rank].votes) \
+                           + " votes)\n "
+        if rank+1 >= max:
+            break
+    if response_string:
+        await response_channel.send(response_string)
+    else:
+        response_channel.send("Something went wrong trying to get the suggestions, sorry.")
+
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -40,6 +66,20 @@ async def on_message(message):
             await message.channel.send("I don't answer to you!")
             return
         print("My master is talking to me on the " + message.guild.name + " server.")
+        max_results = 10
+        match = re.search(r'top (\d+)', message.content)
+        if match and match.group(1):
+            max_results = int(match.group(1))
+        await fetch_suggestions(message.guild, message.channel, max_results)
+
+
+class Suggestion:
+    def __init__(self, votes, name):
+        self.votes = votes
+        self.name = name
+
+    def __str__(self):
+        return self.name + ": " + str(self.votes) + " votes"
 
 
 if __name__ == "__main__":
